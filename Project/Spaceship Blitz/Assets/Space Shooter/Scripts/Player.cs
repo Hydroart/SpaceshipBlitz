125e217b287d303bfcb76185559c051e89591bfb
waterevenant@gmail.com <99618775+Hydroart@users.noreply.github.com>
2023-06-24 03:24:36 +0300

    Uploading version 2 of the project

---
+++ Project/Spaceship Blitz/Assets/Space Shooter/Scripts/Player.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// This script defines which sprite the 'Player" uses and its health.
+/// </summary>
+
+public class Player : MonoBehaviour
+{
+    public float health;
+
+    public GameObject destructionFX;
+
+    public static Player instance; 
+
+    public GameOverScreen GameOverScreen;
+
+    private void Awake()
+    {
+        if (instance == null) 
+            instance = this;
+    }
+
+    //method for damage proceccing by 'Player'
+    public void GetDamage(int damage)   
+    {
+        Instantiate(destructionFX, transform.position, Quaternion.identity);
+        health--;
+        if(health == 0) Destruction();
+    }    
+
+    //'Player's' destruction procedure
+    void Destruction()
+    {
+        Instantiate(destructionFX, transform.position, Quaternion.identity); //generating destruction visual effect and destroying the 'Player' object
+        Destroy(gameObject);
+
+        GameOverScreen.Setup(0);
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
